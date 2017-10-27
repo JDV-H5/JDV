5322278c8c7fda2bf4c1d9962eb76467ea220669
mings6 <1510622081mings6@users.noreply.github.com>
2017-10-27 22:20:38 +0800

    10.27  lsm

--- src/js/index.js
+++ src/js/index.js
@@ -15,10 +15,21 @@ Room.Index.dom = function(){
     });
 
 };
+Room.Index.coming = function(){
+    $("#Index .topWheel").velocity({ rotateZ:["360deg","0deg"] }, { easing:"linear", duration: 2000, loop: true});
+    $("#Index .bottomWheel").velocity({ rotateZ:["360deg","0deg"] }, { easing:"linear", duration: 2000, loop: true});
+};
+
+Room.Index.go_after = function () {
+    $("#Index .topWheel").velocity("stop");
+    $("#Index .bottomWheel").velocity("stop");
+};
+
 Room.Index.ppt = function(){
     cc.ppt(["Index", "GameLoad1"] , function(after , callback){
         cc.m["Index"].velocity({ opacity: 0}, { duration: 1000, display: "none" });
         cc.m["GameLoad1"].css({"opacity": 0}).show().velocity({ opacity: 1}, { duration: 1000, complete:function(){
+            after.go();
             GameLoad1();
         }});
     })
